d0b9ed813850320eb43dda6a14ee10be163e5dec
joq62 <joakim.leche@gmail.com>
2021-10-03 13:55:29 +0200

    added dbase_dist

--- joq_start.sh
+++ joq_start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-rm -rf controller
-git clone https://github.com/joq62/controller.git
-/lib/erlang/bin/erl -pa controller/ebin -pa apps/*/ebin -sname controller -setcookie c1 -detached -run controller boot 
+rm -rf dbase_dist
+git clone https://github.com/joq62/dbase_dist.git
+/lib/erlang/bin/erl -pa dbase_dist/ebin -sname dbase_dist -setcookie cookie -detached -run dbase_dist boot 
